6e7e360d37955e0fb6046b1b3f91cc927230a3d5
prp00328evonne <prp00328evonne@gmail.com>
2022-07-28 13:46:46 +0800

    css upload

--- index/format.php
+++ index/format.php
@@ -163,6 +163,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>筆記分享區</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body bgcolor=F0F0F0>
 

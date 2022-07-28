282dae6713f965d64ce079b448f6d225598cc775
prp00328evonne <prp00328evonne@gmail.com>
2022-07-28 16:07:31 +0800

    new-bar upload

---
+++ new-bar/new-bar.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>筆記分享區</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>  
+
+    <link rel="stylesheet" href="change.css">
+</head>
+<body>
+
+<!--bar-->
+
+<nav class="navbar-expand-lg">
+        <div class ="container-fluid bar navbar">
+            <!--logo -->
+            <a class="navbar-brand" href="#"><img width="175px" height="60px" src="pic/ezlogo.png"></a>
+            <!-- 手機版 -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#linkbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- 電腦版 -->
+            <div class="collapse navbar-collapse" id="linkbar">
+                <!-- 列出內容 -->
+                <ul class="navbar-nav me-auto">
+
+                    <form class="form-inline" role="search" action="/Search/SearchList" method="get" target="_blank">
+                        <div class="form-group">
+                        <input class="navbar-beauty" list="theme" id="choice" name="choice" placeholder="搜尋標籤..."/>
+                        <datalist id="theme">
+                            <option value="Python">
+                            <option value="Php">
+                            <option value="Java">
+                                            
+                        </datalist>    
+                        </div>
+                        
+                        
+                    </form>
+                    <li class="nav-item">
+                    <button class="nav-link search-btn" type="submit" ><i class="fas fa-search"></i></button>
+                    </li>
+                </ul>
+
+                <!-- 列在右邊 -->
+                <ul class="navbar-nav nav-right" >
+                <li class="nav-item">
+                    <a class="nav-link" style="position:absolute; bottom:5px; left:1100px; font-weight:bold; font-size:20px;" href="#">問答區</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" style="position:absolute;  bottom:5px; left:1210px; font-weight:bold; font-size:20px;" href="#">我的角色</a>
+                </li>
+
+                <li class="nav-item">
+                    <button class="btn add-post-btn" type="submit" href="#">+</button>
+                </li>
+                </ul>
+            </div>
+
+            </div>
+    </nav>
+    
+    </body>
+</html>

71802f084aafb1565df6d624f288ea099bc87422
prp00328evonne <prp00328evonne@gmail.com>
2022-07-28 15:39:09 +0800

    外觀2.0

--- index/format.php
+++ index/format.php
@@ -9,15 +9,16 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
+  <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>  
+
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
 
 
 <!--bar-->
-<nav class="navbar navbar-expand-lg navbar-light">
-        <div class ="container-fluid">
+<nav class="navbar-expand-lg">
+        <div class ="container-fluid bar navbar">
             <!--logo -->
             <a class="navbar-brand" href="#"><img width="175px" height="60px" src="pic/ezlogo.png"></a>
             <!-- 手機版 -->
@@ -31,43 +32,41 @@
 
                     <form class="form-inline" role="search" action="/Search/SearchList" method="get" target="_blank">
                         <div class="form-group">
-                        <input style="position:absolute; bottom:15px; left:300px; width: 700px; height:40px; border:2px solid #FFE66F;" list="theme" id="choice" name="choice" placeholder="搜尋標籤..."/>
+                        <input class="navbar-beauty" list="theme" id="choice" name="choice" placeholder="搜尋標籤..."/>
                         <datalist id="theme">
                             <option value="Python">
                             <option value="Php">
                             <option value="Java">
                                             
-                        </datalist>
-                        <button class="btn my-5 my-sm-0" type="submit" style="background:none; margin-left:-3rem; color:cornflowerblue" >
-                        <i class="zi zi_search" ></i></button>
+                        </datalist>    
                         </div>
                         
                         
                     </form>
                     
-                    
+                    <button class="search-btn" type="submit" ><i class="fas fa-search"></i></button>
                     
                 </ul>
 
                 <!-- 列在右邊 -->
                 <ul class="navbar-nav nav-right" >
                 <li class="nav-item">
-                    <a class="nav-link" style="position:absolute; bottom:5px; left:1100px;" href="#">問答區</a>
+                    <a class="nav-link" style="position:absolute; bottom:5px; left:1100px; font-weight:bold;" href="#">問答區</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" style="position:absolute;  bottom:5px; left:1210px;" href="#">我的角色</a>
+                    <a class="nav-link" style="position:absolute;  bottom:5px; left:1210px; font-weight:bold;" href="#">我的角色</a>
                 </li>
 
                 <li class="nav-item">
-                    <button class="btn btn-success" style="position:absolute;  bottom:10px; left:1350px;" type="submit" href="#">+</button>
+                    <button class="btn add-post-btn" type="submit" href="#">+</button>
                 </li>
                 </ul>
             </div>
 
+            </div>
+    </nav><br><br><br>
 
-        </div>
-    </nav><br><br>
 
 
 

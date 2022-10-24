a96bcb1fbb47806f668b4e19f6db3a3f7472ab1e
Changweichun <409401485@mail.fju.edu.tw>
2022-10-25 01:40:39 +0800

    Line _LogIn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { SessionProvider } from "next-auth/react";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  //const [status, setStatus] = useState<AUTH_STATUS>(AUTH_STATUS.LOGOUT);
+  return (
+    <SessionProvider session={pageProps.session}>  
+      <Component {...pageProps} />
+    </SessionProvider>
+
+  )
+
 }
 
 export default MyApp

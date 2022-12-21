ecf46497eb1409e99457017f6bf1a6c11b2ce1c7
Yuhanie <108142833+Yuhanie@users.noreply.github.com>
2022-12-21 11:15:35 +0800

    改回api key

--- settings/firebaseConfig.ts
+++ settings/firebaseConfig.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  apiKey:"AIzaSyDpK9585Nist4dDRMyUbxf98APPUeF051g",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // apiKey:"AIzaSyDpK9585Nist4dDRMyUbxf98APPUeF051g",
   authDomain: "ez-group-6808f.firebaseapp.com",
   projectId: "ez-group-6808f",
   storageBucket: "ez-group-6808f.appspot.com",

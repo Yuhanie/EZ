c60285b93720f96896236b9308fcbd5c2455f902
Changweichun <409401485@mail.fju.edu.tw>
2023-01-15 17:09:54 +0800

    註冊登入更

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,10 +1,37 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import React from 'react'
+import { getApps, getApp, initializeApp } from "firebase/app";
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import styles from "/styles/Home.module.css";
+import {firebaseConfig} from '../../settings/firebaseConfig';
 
-const Home: NextPage = () => {
+export default function SignUp() {
+  if (getApps().length===0) {
+    initializeApp(firebaseConfig);
+  }
+  const [account, setAccount] = useState({email:"",password:"", displayName:""});
+  const [message, setMessage] = useState("");
+  const handleChange = function(e){
+    setAccount({...account,[e.target.name]:e.target.value})
+  }
+  const handleSubmit = async function(){
+    try {
+      const auth = getAuth();
+      const res = await createUserWithEmailAndPassword(auth, account.email, account.password);
+      if (res) {
+        await updateProfile(auth.currentUser,{displayName: account.displayName});
+      }
+      setMessage("");
+
+    }
+    catch(error){
+      setMessage(""+error);
+    }
+  }
+
+  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -60,5 +87,3 @@ const Home: NextPage = () => {
     </div>
   )
 }
-
-export default Home

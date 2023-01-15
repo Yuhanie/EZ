3a9101809ce655bb88e378530b88070df3f40b36
minnn <mz72366816@gmail.com>
2023-01-15 20:19:48 +0800

    MultipleSelectChip （還沒成功

--- pages/addArticle/addArticle.tsx
+++ pages/addArticle/addArticle.tsx
@@ -4,9 +4,11 @@ import Image from 'next/image'
 import React from 'react'
 import styles from "/styles/Home.module.css";
 import Link from 'next/link'
-import { Button, TextField, Container, Autocomplete, Stack, Box } from '@mui/material';
-
-
+import {
+  Button, TextField, Container, Autocomplete, Stack, Box, Select, MenuItem,
+  FormControl, OutlinedInput, Chip, InputLabel, SelectChangeEvent,
+} from '@mui/material';
+import { Theme, useTheme } from '@mui/material/styles';
 
 <link
   rel="stylesheet"
@@ -16,7 +18,51 @@ import { Button, TextField, Container, Autocomplete, Stack, Box } from '@mui/mat
 />
 
 
+
+
 const Home: NextPage = () => {
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const names = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+  ];
+
+  function getStyles(name: string, personName: readonly string[], theme: Theme) {
+    return {
+      fontWeight:
+        personName.indexOf(name) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+  }
+
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -29,7 +75,7 @@ const Home: NextPage = () => {
 
         <img className={styles.user_image} src="pic/test1.jpeg" />
 
-        <Box sx={{margin: 4, }}>
+        <Box sx={{ margin: 4, }}>
           <TextField id="outlined-basic" label="請輸入文章標題" variant="outlined" /><br />
           <TextField
             id="outlined-multiline-static"
@@ -38,8 +84,53 @@ const Home: NextPage = () => {
             fullWidth
             rows={10}
             margin="normal"
-            
+
           /><br />
+          <div>
+          <h4>請選擇標籤</h4>
+          <FormControl sx={{ m: 0.5, width: 300 }}>
+          <Select
+                labelId="demo-multiple-chip-label"
+                id="demo-multiple-chip"
+              >
+                <MenuItem value="note">課堂筆記</MenuItem>
+                <MenuItem value="diary">修課心得</MenuItem>
+                <MenuItem value="project">專題相關</MenuItem>
+                <MenuItem value="company">業界資源</MenuItem>
+                <MenuItem value="other">其他</MenuItem>
+              </Select><br />
+           </FormControl>
+            <FormControl sx={{ m: 0.5, width: 300 }}>
+            
+              <InputLabel id="demo-multiple-chip-label">標籤</InputLabel>
+              <Select
+                labelId="demo-multiple-chip-label"
+                id="demo-multiple-chip"
+                
+                onChange={handleChange}
+                input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                renderValue={(selected) => (
+                  <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                    {selected.map((value) => (
+                      <Chip key={value} label={value} />
+                    ))}
+                  </Box>
+                )}
+                MenuProps={MenuProps}
+              >
+                {names.map((name) => (
+                  <MenuItem
+                    key={name}
+                    value={name}
+                    style={getStyles(name, personName, theme)}
+                  >
+                    {name}
+                  </MenuItem>
+                ))} 
+              </Select>
+            </FormControl>
+          </div>
+
           <Button color="primary" ><input multiple type="file" /></Button>
 
           <div>
@@ -53,7 +144,7 @@ const Home: NextPage = () => {
 
 
 
-      
+
 
       {/* <footer className={styles.footer}>
         <Link

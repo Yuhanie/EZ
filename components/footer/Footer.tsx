b0f228e9d3cbc04bbd4ee2194a9c9909459232d3
minnn <mz72366816@gmail.com>
2023-03-08 00:10:50 +0800

    footer完成

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,9 +5,11 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from "@mui/material/Typography";
-import { Image } from "@mui/icons-material";
+import Image from "next/image";
 import { Card, ImageListItem } from "@mui/material";
 import Button from "@mui/material/Button";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import welcome2 from "../../public/pic/welcome2.png"
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -30,7 +32,7 @@ export default function Footer() {
   return (
     <footer>
       <Box
-        bgcolor="#000000"
+        bgcolor="#fafafa"
         p={2}
 
       >
@@ -38,31 +40,46 @@ export default function Footer() {
           display="flex"
           justifyContent="center"
         >
-          <Grid container spacing={4} maxWidth="80%" display="flex" flexWrap="wrap">
+          <Grid container maxWidth="80%" display="flex" flexWrap="wrap">
             <Grid item xs={4} minWidth={300}>
-              <Item sx={{ height: 150 }} >
-                <Box display="flex">
-                  <Grid xs={7}>
-                    <Typography variant="h6" sx={{ p: 1,textAlign:"left" }}>官方帳號</Typography>
-                    <Typography variant="button" display="block" gutterBottom sx={{ p: 1,textAlign:"left" }}>加入我們！<br/>讓學習變得更easy</Typography>
-                  </Grid>
-                  <Grid sx={{ width: 130 }}>
-                    {itemData.map((item) => (
-                      <ImageListItem key={item.img}>
-                        <img
-                          src={`${item.img}?w=100&fit=crop&auto=format`}
-                        />
-                      </ImageListItem>
-                    ))}
-                  </Grid>
+              <Box display="flex" sx={{ height: 150 }}>
+                <Grid xs={7}>
+                  <Typography variant="h6" sx={{ p: 1, textAlign: "left" }}>官方帳號</Typography>
+                  <Typography variant="button" display="block" gutterBottom sx={{ p: 1, textAlign: "left", href: "https://lin.ee/qh8TtVP" }}>加入我們！<br />讓學習變得更easy</Typography>
+                </Grid>
+                <Grid sx={{ width: 130 }}>
+                  {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                      <img
+                        src={`${item.img}?w=100&fit=crop&auto=format`}
+                      />
+                    </ImageListItem>
+                  ))}
+                </Grid>
+              </Box>
+            </Grid>
+
+            <Grid item xs={4} minWidth={300}>
+              <Box sx={{ height: 140 }}>
+                <Typography variant="h6" sx={{ p: 1, textAlign: "left" }}>聯絡我們</Typography>
+                <Box display="flex" m={1} >
+                  <MailOutlineIcon sx={{mr:2}} />
+                  <Typography>educatineZone@gmail.com</Typography> 
                 </Box>
-              </Item>
+                <Typography variant="body2" sx={{m:1}}>多利用Line官方帳號提問更便利！</Typography>
+              </Box>
             </Grid>
-            <Grid item xs={4}>
-              <Item>2</Item>
-            </Grid>
-            <Grid item xs={4}>
-              <Item>3</Item>
+            <Grid item xs={4} minWidth={350}>
+              <Box width="1" height={100}>
+                <Image
+                  src={welcome2}
+                  alt="welcome"
+                  width={350}
+                  height={170}
+                  objectFit="none"
+                  objectPosition="top"
+                   />
+              </Box>
             </Grid>
           </Grid>
         </Box>

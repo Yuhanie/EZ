139abaa009d7d11808d123772fde718f4771538d
409401394 <yuweiliao0520@gmail.com>
2023-03-23 16:19:42 +0800

    增加firebase測試資料

--- settings/firebaseConfig.ts
+++ settings/firebaseConfig.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // apiKey:"AIzaSyDpK9585Nist4dDRMyUbxf98APPUeF051g",
-  authDomain: "ez-group-6808f.firebaseapp.com",
-  projectId: "ez-group-6808f",
-  storageBucket: "ez-group-6808f.appspot.com",
-  messagingSenderId: "747617601049",
-  appId: "1:747617601049:web:f158c5e83fadb3505a0c88",
-  measurementId: "G-PGXTMCCV8T"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };

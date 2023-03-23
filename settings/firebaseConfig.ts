5e7035db3e6f017b04fbf60fc28bc7b0695b14b3
409401394 <yuweiliao0520@gmail.com>
2023-03-23 17:09:31 +0800

    Update firebaseConfig.ts
    problem to be solved~~~~~

--- settings/firebaseConfig.ts
+++ settings/firebaseConfig.ts
@@ -1,18 +1,18 @@
-export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
 // export const firebaseConfig = {
 //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: "ez-group-6808f.firebaseapp.com",
-//   projectId: "ez-group-6808f",
-//   storageBucket: "ez-group-6808f.appspot.com",
-//   messagingSenderId: "747617601049",
-//   appId: "1:747617601049:web:f158c5e83fadb3505a0c88",
-//   measurementId: "G-PGXTMCCV8T"
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 // };
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "ez-group-6808f.firebaseapp.com",
+  projectId: "ez-group-6808f",
+  storageBucket: "ez-group-6808f.appspot.com",
+  messagingSenderId: "747617601049",
+  appId: "1:747617601049:web:f158c5e83fadb3505a0c88",
+  measurementId: "G-PGXTMCCV8T"
+};

7bf9da8ec8708df9cebce076dcc44cd277a34da7
minnn <mz72366816@gmail.com>
2023-04-02 02:28:45 +0800

    ezindex part2(大概50%) 明天繼續

--- pages/ezindex/Ezindex.tsx
+++ pages/ezindex/Ezindex.tsx
@@ -9,6 +9,13 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import workingPerson from "../../public/pic/workingPerson.png";
 import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import NotificationImportantIcon from '@mui/icons-material/NotificationImportant';
+import WarningIcon from '@mui/icons-material/Warning';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import Card from "@mui/material/Card";
+import VI from "@mui/icons-material/Visibility";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -43,7 +50,7 @@ const Ezindex = () => {
                         <Grid item>
 
                             <Box display="flex" flexWrap="wrap" justifyContent="center" alignItems="center">
-                                <Box display="flex" flexDirection="column" justifyContent="space-between"  maxWidth={400} sx={{ p: 4 }}>
+                                <Box display="flex" flexDirection="column" justifyContent="space-between" maxWidth={400} sx={{ p: 4 }}>
                                     {/* 網頁 */}
                                     <Typography
                                         variant="h2"
@@ -55,7 +62,7 @@ const Ezindex = () => {
                                             letterSpacing: ".3rem",
                                             color: "inherit",
                                             textDecoration: "none",
-                                            pb:3
+                                            pb: 3
                                         }}
                                     >
                                         讓學習變得<br />更Easy
@@ -65,7 +72,7 @@ const Ezindex = () => {
                                         variant="h3"
                                         noWrap
                                         component="a"
-                                       
+
                                         sx={{
                                             display: { xs: "flex", md: "none" },
                                             flexGrow: 1,
@@ -73,15 +80,15 @@ const Ezindex = () => {
                                             letterSpacing: ".3rem",
                                             color: "inherit",
                                             textDecoration: "none",
-                                            pb:3
+                                            pb: 3
                                         }}
                                     >
                                         讓學習變得<br />更Easy
                                     </Typography>
 
-                                    <Typography variant="body1" sx={{ textAlign: "left",letterSpacing:".1rem",lineHeight:1.8,color:"#000000" }}>以「Tag、Link、Update」優化文章閱讀體驗，快速獲得正確知識，讓學習更省時、更高效。</Typography>
+                                    <Typography variant="body1" sx={{ textAlign: "left", letterSpacing: ".1rem", lineHeight: 1.8, color: "#000000" }}>以「Tag、Link、Update」優化文章閱讀體驗，快速獲得正確知識，讓學習更省時、更高效。</Typography>
                                     {/* <Typography sx={{ textAlign: "left", }}>獲取最新的資訊</Typography> */}
-                                    <Button variant="contained" color="secondary" href="/" sx={{mt:7}}>文章分享區</Button>
+                                    <Button variant="contained" color="secondary" href="/" sx={{ mt: 7 }}>筆記分享區</Button>
                                 </Box>
                                 <Toolbar />
                                 <Box sx={{ maxWidth: 600, p: 4 }}>
@@ -91,16 +98,69 @@ const Ezindex = () => {
 
                         </Grid>
 
-                        {/* <Grid item minWidth={300}>
-                        <Box sx={{ height: 140 }}>
-                            <Typography variant="h6" sx={{ p: 1, textAlign: "left" }}>聯絡我們</Typography>
-                            <Box display="flex" m={1} >
+                        <Grid item minWidth={300}>
+                            <Box display="flex" flexWrap="wrap" >
 
-                                <Typography>educationZone@gmail.com</Typography>
+                                <Grid xs={6} sx={{ p: 4 }}>
+                                    <Box display="flex" flexDirection="column" flexWrap="wrap">
+                                        <Typography variant="h6">文章狀態</Typography>
+                                        <Box display="flex" alignItems="center">
+                                            <IconButton ><CheckCircleIcon sx={{ color: "Green" }} /></IconButton>
+                                            <Typography>正確</Typography>
+                                        </Box>
+                                        <Box display="flex" alignItems="center">
+                                            <IconButton><NotificationImportantIcon sx={{ color: "Gold" }} /></IconButton>
+                                            <Typography>審查中</Typography>
+                                        </Box>
+                                        <Box display="flex" alignItems="center" >
+                                            <IconButton><WarningIcon sx={{ color: "Crimson" }} /></IconButton>
+                                            <Typography>版本疑慮</Typography><br />
+                                        </Box>
+                                        <Typography variant="caption">經由專家審查後，確認文章版本過時或無法使用</Typography>
+                                    </Box>
+
+                                </Grid>
+                                <Grid xs={3}>
+                                    <Card sx={{ width: 350, height: 500, p: 2 }}>
+                                        <Box display="flex" alignItems="center" justifyContent="space-between">
+                                            <Box display="flex">
+                                                <WarningIcon />
+                                                <Typography>文章標題</Typography>
+                                            </Box>
+                                            <Box display="flex">
+                                                <VI  />
+                                                <Typography variant="body2">10</Typography>
+                                                <MoreHorizIcon sx={{bgcolor:"#FFF6E1",borderRadius:10}}/>
+                                            </Box>
+                                        </Box>
+                                        <Box sx={{ height: 150, bgcolor: "#fafafa", p: 2, m: 2 }}>
+                                            <Typography>文章內容</Typography>
+                                        </Box>
+                                        <Box sx={{ height: 100, bgcolor: "#fafafa", p: 2, m: 2 }}>
+                                            <Typography>文章過時</Typography>
+                                        </Box>
+
+
+
+                                    </Card>
+                                </Grid>
+                                <Grid xs={3}>
+                                    <Box display="flex" flexDirection="column" sx={{m:2,pl:4}}>
+                                        <Box sx={{m:1,p:2,height:150,bgcolor:"#FFF6E1"}}>
+                                            <Typography variant="h6">學習者</Typography>
+                                        </Box>
+                                        <Box sx={{m:1,p:2,height:150,bgcolor:"#E9F0E7"}}>
+                                            <Typography variant="h6">專家</Typography>
+                                        </Box>
+
+                                    </Box>
+
+                                </Grid>
+
+
+
                             </Box>
-                            <Typography variant="body2" sx={{ m: 1 }}>多利用Line官方帳號提問更便利！</Typography>
-                        </Box>
-                    </Grid> */}
+                        </Grid>
 
                     </Grid>
                 </ThemeProvider>

042bb08cf6eb91e9807fd786523158590e79b503
409401394 <yuweiliao0520@gmail.com>
2023-04-08 23:18:20 +0800

    第二次挑戰

--- myapp/src/firebase.js
+++ myapp/src/firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+//import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+
 const firebaseConfig = {
 	apiKey: "AIzaSyDpK9585Nist4dDRMyUbxf98APPUeF051g",
     authDomain: "ez-group-6808f.firebaseapp.com",
@@ -10,12 +13,15 @@ const firebaseConfig = {
     appId: "1:747617601049:web:f158c5e83fadb3505a0c88",
     measurementId: "G-PGXTMCCV8T"
 };
+if (!firebase.apps.length) {
+    firebase.initializeApp({});
+}
 
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app); 
+const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-export {auth, provider};
+export { auth, provider};
 
 // var storage = firebase.storage();
 // export default storage;

8c8ce417df7b2e5bdfd5452df65200efd0b1e1af
minnn <mz72366816@gmail.com>
2023-04-09 03:00:53 +0800

    Icon

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import styles from "/styles/Home.module.css";
 import Index from "pages";
+import googleIcon from "../../public/pic/googleIcon.png";
 import googleSignin from "../../components/googleSignin/signin";
 import Note from "../../pages/note"
 //firebase

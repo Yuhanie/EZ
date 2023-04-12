a9a542d59497f096eb48126fd7c493e23b17dda0
prp00328evonne <prp00328evonne@gmail.com>
2023-04-12 14:33:10 +0800

    15-1 寫死

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -71,7 +71,8 @@ ElevationScroll.propTypes = {
 const pages = [
   { text: "首頁", href: "/" },
   { text: "筆記分享區", href: "/note" },
-  // { text: "問答區", href: "" },
+  { text: "問答區", href: "" },
+  { text: "審查筆記", href:""}
 
 ];
 const settings = [
@@ -297,11 +298,11 @@ function ResponsiveAppBar() {
                     </Typography>
                   </MenuItem>
                 ))}
-                <MenuItem>
+                {/* <MenuItem>
                   <Typography>
                     問答區
                   </Typography>
-                </MenuItem>
+                </MenuItem> */}
               </Menu>
             </Box>
 
@@ -343,14 +344,14 @@ function ResponsiveAppBar() {
                   </Typography>
                 </Button>
               ))}
-
+{/* 
               <Tooltip title="即將登場！">
                 <Button sx={{ my: 2, color: "black", display: "block" }}>
                   <Typography>
                     問答區
                   </Typography>
                 </Button>
-              </Tooltip>
+              </Tooltip> */}
 
             </Box>
 

8d8908658446c523c1fa70a725dea7b56e507876
Ben Wu <jitsung.wu@gmail.com>
2023-04-17 21:27:22 +0800

    Flex Message

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import {
   Client,
+  FlexMessage,
   MessageAPIResponseBase,
   TextMessage,
   WebhookEvent,
@@ -60,12 +61,72 @@ const textEventHandler = async (
   const { replyToken } = event;
   const { text } = event.message;
 
-  // Create a new message.
+  //Create a new message.
   const response: TextMessage = {
     type: "text",
     text,
   };
 
+  const responseNotes: FlexMessage = {
+    type: "flex",
+    altText: text,
+    contents: {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+        action: {
+          type: "uri",
+          label: "link",
+          uri: "http://linecorp.com/",
+        },
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: "吳老師的Next筆記",
+            weight: "bold",
+            size: "xl",
+          },
+        ],
+      },
+      footer: {
+        type: "box",
+        layout: "vertical",
+        spacing: "sm",
+        contents: [
+          {
+            type: "button",
+            style: "link",
+            height: "sm",
+            action: {
+              type: "uri",
+              label: "WEBSITE",
+              uri: "https://fju-benwu.notion.site/Next-548a6c9fed644164ae9562510e30cffb",
+            },
+          },
+          {
+            type: "box",
+            layout: "vertical",
+            contents: [],
+            margin: "sm",
+          },
+        ],
+        flex: 0,
+      },
+    },
+  };
+
   // Reply to the user.
-  await client.replyMessage(replyToken, response);
+  if (text === "筆記") {
+    await client.replyMessage(replyToken, responseNotes);
+  } else {
+    await client.replyMessage(replyToken, response);
+  }
 };

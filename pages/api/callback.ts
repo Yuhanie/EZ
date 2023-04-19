f226eaeef8f15c24e9f2294c2248b0e688f802fc
prp00328evonne <prp00328evonne@gmail.com>
2023-04-19 12:40:45 +0800

    hero byebye

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -72,18 +72,18 @@ const textEventHandler = async (
     altText: text,
     contents: {
       type: "bubble",
-      hero: {
-        type: "image",
-        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-        size: "full",
-        aspectRatio: "20:13",
-        aspectMode: "cover",
-        action: {
-          type: "uri",
-          label: "link",
-          uri: "http://linecorp.com/",
-        },
-      },
+      // hero: {
+      //   type: "image",
+      //   url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+      //   size: "full",
+      //   aspectRatio: "20:13",
+      //   aspectMode: "cover",
+      //   action: {
+      //     type: "uri",
+      //     label: "link",
+      //     uri: "http://linecorp.com/",
+      //   },
+      // },
       body: {
         type: "box",
         layout: "vertical",

530a4c8f2cb2cbb9d89bb48471651806ec00eae9
Ben Wu <jitsung.wu@gmail.com>
2023-04-20 12:59:31 +0800

    test middleware again

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -1,18 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import {
+  ClientConfig,
   Client,
-  FlexMessage,
-  MessageAPIResponseBase,
-  MiddlewareConfig,
-  TextMessage,
-  WebhookEvent,
   middleware,
+  MiddlewareConfig,
+  WebhookEvent,
+  TextMessage,
+  MessageAPIResponseBase,
 } from "@line/bot-sdk";
-const clientConfig = {
+
+// Setup all LINE client configurations.
+const clientConfig: ClientConfig = {
   channelAccessToken: process.env.NEXT_PUBLIC_CHANNEL_ACCESS_TOKEN || "",
-  channelSecret: process.env.NEXT_PUBLIC_CHANNEL_SECRET,
+  channelSecret: process.env.NEXT_PUBLIC_CHANNEL_SECRET || "",
 };
+
 const middlewareConfig: MiddlewareConfig = {
   channelAccessToken: process.env.NEXT_PUBLIC_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.NEXT_PUBLIC_CHANNEL_SECRET || "",
@@ -21,54 +23,8 @@ const middlewareConfig: MiddlewareConfig = {
 // Create a new LINE SDK client.
 const client = new Client(clientConfig);
 
-// type Data = {
-//   status?: string;
-//   // name: string;
-//   results?: any;
-// };
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  middleware(middlewareConfig);
-  const events: WebhookEvent[] = req.body.events;
-  console.log(events);
-  // const replyToken = req.body.events[0].replyToken;
-  // const response: TextMessage = {
-  //   type: "text",
-  //   text: "test",
-  // };
-  // // const response = {
-  // //   type: "text",
-  // //   text: "test",
-  // // };
-  // await client.replyMessage(replyToken, response);
-
-  // res.send("HTTP POST request sent to the webhook URL!");
-  // Process all of the received events asynchronously.
-
-  const results = events.map(async (event: WebhookEvent) => {
-    try {
-      await textEventHandler(event);
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        console.error("Error in textEventHandler:", err);
-      }
-
-      // Return an error message.
-      return res.status(500).json({
-        status: "error",
-      });
-    }
-  });
-
-  // Return a successfull message.
-  return res.status(200).json({
-    status: "success",
-    // results,
-  });
-}
+// Middleware to validate the request comes from LINE.
+const lineMiddleware = middleware(middlewareConfig);
 
 // Function handler to receive the text.
 const textEventHandler = async (
@@ -83,78 +39,64 @@ const textEventHandler = async (
   const { replyToken } = event;
   const { text } = event.message;
 
-  //Create a new message.
+  // Create a new message.
   const response: TextMessage = {
     type: "text",
     text,
   };
 
-  const responseNotes: FlexMessage = {
-    type: "flex",
-    altText: text,
-    contents: {
-      type: "bubble",
-      // hero: {
-      //   type: "image",
-      //   url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-      //   size: "full",
-      //   aspectRatio: "20:13",
-      //   aspectMode: "cover",
-      //   action: {
-      //     type: "uri",
-      //     label: "link",
-      //     uri: "http://linecorp.com/",
-      //   },
-      // },
-      body: {
-        type: "box",
-        layout: "vertical",
-        contents: [
-          {
-            type: "text",
-            text: "吳老師的Next筆記",
-            weight: "bold",
-            size: "xl",
-          },
-        ],
-      },
-      footer: {
-        type: "box",
-        layout: "vertical",
-        spacing: "sm",
-        contents: [
-          {
-            type: "button",
-            style: "link",
-            height: "sm",
-            action: {
-              type: "uri",
-              label: "WEBSITE",
-              uri: "https://fju-benwu.notion.site/Next-548a6c9fed644164ae9562510e30cffb",
-            },
-          },
-          {
-            type: "box",
-            layout: "vertical",
-            contents: [],
-            margin: "sm",
-          },
-        ],
-        flex: 0,
-      },
-    },
-  };
-
   // Reply to the user.
-  try {
-    if (text === "筆記") {
-      await client.replyMessage(replyToken, responseNotes);
-    } else {
-      await client.replyMessage(replyToken, response);
-    }
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      console.error("Error in replyMessage:", err);
-    }
-  }
+  await client.replyMessage(replyToken, response);
 };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      await lineMiddleware(req, res, async () => {
+        const events: WebhookEvent[] = req.body.events;
+
+        // Process all of the received events asynchronously.
+        const results = await Promise.all(
+          events.map(async (event: WebhookEvent) => {
+            try {
+              await textEventHandler(event);
+            } catch (err: unknown) {
+              if (err instanceof Error) {
+                console.error(err);
+              }
+
+              // Return an error message.
+              return res.status(500).json({
+                status: "error",
+              });
+            }
+          })
+        );
+
+        // Return a successfull message.
+        return res.status(200).json({
+          status: "success",
+          results,
+        });
+      });
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error(err);
+      }
+
+      // Return an error message.
+      return res.status(500).json({
+        status: "error",
+      });
+    }
+  } else {
+    // Return a successfull message.
+    return res.status(200).json({
+      status: "success",
+      message: "Connected successfully!",
+    });
+  }
+}

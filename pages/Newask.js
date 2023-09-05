a80d360bc62ddc9d1e8fad1c5e86ddc9c2a11128
Changweichun <409401485@mail.fju.edu.tw>
2023-09-05 22:49:35 +0800

    Newask取消鍵

--- pages/Newask.js
+++ pages/Newask.js
@@ -164,6 +164,10 @@ function Newask() {
     setContent(value)
   }
 
+  const back = async function (){
+    router.push('/wishingPool');
+  }
+
   const update = async function () {
     if (content == "" || tagName == "") {
       return (false);
@@ -343,7 +347,7 @@ function Newask() {
           </Box>
         </Box>
         <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <Button variant="contained" disabled color="primary">取消</Button><br></br><br></br>
+          <Button variant="contained" disabled color="primary" onClick={back}>取消</Button><br></br><br></br>
           <Button variant="contained" onClick={update} >送出</Button>
         </CardActions>
 

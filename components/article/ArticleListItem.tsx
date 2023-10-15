f8b91fea83358d6859e335a85b2d9eb06d35be45
prp00328evonne <prp00328evonne@gmail.com>
2023-10-15 14:11:39 +0800

    口以ㄌ

--- components/article/ArticleListItem.tsx
+++ components/article/ArticleListItem.tsx
@@ -274,7 +274,7 @@ const ArticleListItem:
                   <img className={styles.googlephoto_profile} src={profile.photoURL} />} */}
                 </Avatar>
               }
-              title={currentUser&&props.article.userid==currentUser.uid?currentUser.photoURL:props.article.user}
+              title={currentUser&&(props.article.userid==currentUser.uid?currentUser.displayName:props.article.user)}
               subheader={props.article.timestamp && props.article.timestamp.toDate().toLocaleDateString()}
               //item 
               sx={{ p: 1.2 }}

26aca339c6bc9daf451417f51230149a4c5f0898
Changweichun <409401485@mail.fju.edu.tw>
2023-10-25 11:28:15 +0800

    update

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -39,7 +39,7 @@ const handler: NextApiHandler = async (
     });
     return res.status(200).json({ message: "Email成功送出" });
   } catch (error) {
-    return res.status(400).json({ message: "Email無法送出" });
+    return res.status(400).json({ message: "Email無法送出"+error });
     console.error(error);
   }
 
